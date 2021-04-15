24cd0c2b91c4baa3fab4c7c0a858af2c1601753e
Adam Wushensky <adam.wushensky@protonmail.com>
2021-04-15 13:30:03 -0700

    Get android working again

--- example/android/app/src/main/java/com/getbouncer/example/MainApplication.java
+++ example/android/app/src/main/java/com/getbouncer/example/MainApplication.java
@@ -47,5 +47,6 @@ public class MainApplication extends Application implements ReactApplication {
     RNCardVerifyModule.apiKey = "<your_api_key_here>";
     RNCardVerifyModule.enableNameExtraction = true;
     RNCardVerifyModule.enableExpiryExtraction = true;
+    RNCardVerifyModule.useLocalVerificationOnly = true;
   }
 }
